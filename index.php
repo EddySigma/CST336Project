68a70a59ba67d291c96975cb490d5d7f5da6bea9
Miguel Lopez <mnl30@aol.com>
2016-11-06 03:52:21 +0000

    Made a few changes to initial table that is displayed. Added the ability to sort by artist and song name

--- index.php
+++ index.php
@@ -1,150 +1,113 @@
-
-
 <html>
 <title>Music Store</title>
 <style>
-img {
-width:100px;
-height:100px;
-
-}
-table {
-  width:500px;
-  margin: 10px auto;
-  height:auto;
-  width:auto;
- 
-	background-color:white;
-
-}
-
-
-.centerIt
-
-{
-	text-align:center;
-
-}
-
-.myButton {
-	text-align:center;
+	img {
+		width: 100px;
+		height: 100px;
+	}
 	
-}
-
-
- body{
-
-    background-image: url("http://media.gettyimages.com/videos/grunge-music-background-blue-video-id473008825?s=640x640");
-
-   }
-   
- .heading
- {
-	 text-align:center;
-	 background-color:green;
- }
-
-.formBox 
-{
-	text-align:center;
-	background-color:white;
-}
-
+	table {
+		width: 500px;
+		margin: 10px auto;
+		height: auto;
+		width: auto;
+		background-color: white;
+	}
+	
+	.centerIt {
+		text-align: center;
+	}
+	
+	.myButton {
+		text-align: center;
+	}
+	
+	body {
+		background-image: url("http://media.gettyimages.com/videos/grunge-music-background-blue-video-id473008825?s=640x640");
+	}
+	
+	.heading {
+		text-align: center;
+		background-color: green;
+	}
+	
+	.formBox {
+		text-align: center;
+		background-color: white;
+	}
 </style>
-
-</head>
-
-<body>
-<div class='heading'><h1>Top Singles - Shopping Cart</h1></div>
-<div class="centerIt"><form><table border=1px;>
-
-<div class='formBox'>
-
-Search Name: <input type="text" name="FirstName" value=""><br>
-Search Artist: <input type="text" name="LastName" value=" "><br>
-
-
-
-
-
-Max Length (in minutes): <input type="text" name="LastName" value=""><br>
-<input type="submit" value="Submit">
-</div>	
 
 <?php
 
 
 	$servername = "localhost";
 	$username = "root";
-	$password = "yourPassWordHere";
-	
+	$password = "";
 	
 	// dbName = songDB
 	
-	$dbConn = new PDO("mysql:host=$servername;dbname=songDB", $username, $password);
+	$dbConn = new PDO("mysql:host=$servername;dbname=Group_Project", $username, $password);
 	// set the PDO error mode to exception
 	$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = "SELECT * FROM songs ORDER BY artistName";
-	$stmt = $dbConn -> prepare ($sql);
-	$stmt -> execute ();
 	
-
-
-	    echo "<table border=2px;>";
-	    $i = 1;
-	    
-		$labels = array('Album Art','Artist','Song Name','Genre','Length','Price','Add to Shopping Cart');
-		
-		for($i = 0; $i < count($labels); $i++)
-			echo "<th>".$labels[$i]."</th>";
-
-        while ($row = $stmt -> fetch()) {
-			
-			echo "<tr>";
-			
-			if($i % count($labels) == 0)
-			{
-				echo "</tr><tr>";
-			}
-			//` (`albumLink` `artistName`  `songName`  `genre`  `songLength`  );
-			//var_dump($row);
-			echo "<td><img src='". $row['albumLink']. "'></td>";
-			echo "<td>". $row['artistName'] . "</td>";
-			echo "<td>".$row['songName']."</td>";
-			echo "<td>".$row['genre']."</td>";
-			echo "<td>".$row['songLength']."</td>";
-			echo "<td>$".rand(1,3). ".99</td>";
-			
-			$i++;
-			echo "<td><input type='checkbox'></td>";
-		}
-       echo "</table>";
-
-/*
-
-for i in range(40):
-    print('<tr>')
-
-    if((i + 1) % 3 == 0):
-        print('</tr><tr>')
-
-    openAndClose('<img src="' + pictureLinks[i] + '">')
-    openAndClose(artists[i])
-    openAndClose(songNames[i])
-    openAndClose(genres[randint(0,len(genres) - 1)])
-    openAndClose(str(randint(2,5)) + ":" + str(randint(10,59)))
-    openAndClose('<input type="checkbox">')
-
-
-print('</table></form></div></html>')
-*/
-
+	    if (empty($_GET['sort']))   
+        $sort = "order by artistName";
+    else if($_GET['sort'] == 'song'){
+        $sort = "order by songName";
+    }
+    
 
 ?>
-<div class='myButton'><button type="submit" form="nameform" value="Submit"  style="height:80px;width:180px">Buy Now </button></div>
-</form>
+</head>
+
+<body>
+	<div class='heading'>
+		<h1>Top Singles - Shopping Cart</h1>
+	</div>
+	<div class="centerIt">
+		<form>
+			<div>
+
+				Search Name: <input type="text" name="s_name" value=""><br> 
+				Search Artist: <input type="text" name="LastName" value=" "><br>
+				Max Length (in minutes): <input type="text" name="LastName" value=""><br>
+				<input type="submit" value="Submit">
+			</div>
+			
+		</form>				
+		<table border=2px>
+			<thead>
+				<tr></tr>
+					<th>Album Art</th>
+					<th><a href=index.php?sort=artist>Artist</a></th>
+					<th><a href=index.php?sort=song>Song Name</a></th>
+					<th>Price</th>
+					<th>Add to Shopping Cart</th>
+			</thead>
+			<tbody>
+			<?php
+
+				$sql = "SELECT albumLink, artistName, songName 
+				FROM songs " . $sort;
+				
+				$stmt = $dbConn -> prepare ($sql);
+				$stmt -> execute ();
+	
+				while ($row = $stmt -> fetch()) {
+			
+					echo "<tr>";
+					echo "<td><img src='" . $row['albumLink']. "'></td>";
+					echo "<td>" . $row['artistName'] . "</td>";
+					echo "<td>" . $row['songName'] . "</td>";
+					echo "<td>$" . rand(1,3) . ".99</td>"; 
+					echo "<td><input type='button' value='Add to Shopping Cart'></td>";
+				}
+
+			?>
+			</tbody>
+		</table>
+	</div>
+
 </body>
 
 </html>
-
-
